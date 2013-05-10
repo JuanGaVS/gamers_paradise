75e979754fc2c773c73d13c79e55475b14b27608
Pablo Roberto Gonzalez Herrera <pablorobertogonzalezh@gmail.com>
2013-05-10 00:55:17 -0600

    cambio de extension archivo instrucciones

---
+++ admin/add_user.php
@@ -0,0 +1,133 @@
+<?php require_once('../Connections/localhost.php'); ?>
+<?php require_once('../entities/User.php'); ?>
+<?php
+if (!function_exists("GetSQLValueString")) {
+function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") 
+{
+  if (PHP_VERSION < 6) {
+    $theValue = get_magic_quotes_gpc() ? stripslashes($theValue) : $theValue;
+  }
+
+  $theValue = function_exists("mysql_real_escape_string") ? mysql_real_escape_string($theValue) : mysql_escape_string($theValue);
+
+  switch ($theType) {
+    case "text":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;    
+    case "long":
+    case "int":
+      $theValue = ($theValue != "") ? intval($theValue) : "NULL";
+      break;
+    case "double":
+      $theValue = ($theValue != "") ? doubleval($theValue) : "NULL";
+      break;
+    case "date":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;
+    case "defined":
+      $theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
+      break;
+  }
+  return $theValue;
+}
+}
+
+$editFormAction = $_SERVER['PHP_SELF'];
+if (isset($_SERVER['QUERY_STRING'])) {
+  $editFormAction .= "?" . htmlentities($_SERVER['QUERY_STRING']);
+}
+
+if ((isset($_POST["MM_insert"])) && ($_POST["MM_insert"] == "form1")) {
+	$username = $_POST['username'];
+	$first_name = $_POST['first_name'];
+	$last_name = $_POST['last_name'];
+	$email = $_POST['email'];
+	$password = $_POST['password'];
+	$user = new User();
+	$user->setUsername($username);
+	$user->setFirst_name($first_name);
+	$user->setLast_name($last_name);
+	$user->setEmail($email);
+	$user->setSalt();
+	$user->setPassword($password);
+	$user->userAdd();
+	
+	/*
+	$insertSQL = sprintf("INSERT INTO tb_users (username, first_name, last_name, email, password, salt) VALUES (%s, %s, %s, %s, %s, %s)",
+                       GetSQLValueString($_POST['username'], "text"),
+                       GetSQLValueString($_POST['first_name'], "text"),
+                       GetSQLValueString($_POST['last_name'], "text"),
+                       GetSQLValueString($_POST['email'], "text"),
+                       GetSQLValueString($_POST['password'], "text"));
+
+  mysql_select_db($database_localhost, $localhost);
+  $Result1 = mysql_query($insertSQL, $localhost) or die(mysql_error());
+  */
+
+  $insertGoTo = "users.php";
+  if (isset($_SERVER['QUERY_STRING'])) {
+    $insertGoTo .= (strpos($insertGoTo, '?')) ? "&" : "?";
+    $insertGoTo .= $_SERVER['QUERY_STRING'];
+  }
+  header(sprintf("Location: %s", $insertGoTo));
+}
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+<script src="../SpryAssets/SpryValidationPassword.js" type="text/javascript"></script>
+<link href="../SpryAssets/SpryValidationPassword.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+<form id="form_add_user" name="form1" method="POST" action="<?php echo $editFormAction; ?>">
+  <table width="309" border="1">
+    <tr>
+      <th width="191" scope="row">Usuario</th>
+      <td width="102"><label for="username"></label>
+      <input type="text" name="username" id="username" /></td>
+    </tr>
+    <tr>
+      <th scope="row">Nombre</th>
+      <td><label for="first_name"></label>
+      <input type="text" name="first_name" id="first_name" /></td>
+    </tr>
+    <tr>
+      <th scope="row">Apellido</th>
+      <td><label for="last_name"></label>
+      <input type="text" name="last_name" id="last_name" /></td>
+    </tr>
+    <tr>
+      <th scope="row">Email</th>
+      <td><label for="email"></label>
+      <input type="text" name="email" id="email" /></td>
+    </tr>
+    <tr>
+      <th scope="row">Contraseña</th>
+      <td><span id="sprypassword1">
+        <label for="password"></label>
+        <input type="password" name="password" id="password" />
+      <span class="passwordRequiredMsg">A value is required.</span></span></td>
+    </tr>
+    <tr>
+      <th scope="row">Confirmación Contraseña</th>
+      <td><span id="sprypassword2">
+        <label for="password_retype"></label>
+        <input type="password" name="password_retype" id="password_retype" />
+      <span class="passwordRequiredMsg">A value is required.</span></span></td>
+    </tr>
+    <tr>
+      <th colspan="2" scope="row"><input type="submit" name="button_add" id="button_add" value="Agregar" />
+      <input type="reset" name="button_reset" id="button_reset" value="Limpiar" /></th>
+    </tr>
+  </table>
+  <input type="hidden" name="MM_insert" value="form1" />
+</form>
+<script type="text/javascript">
+var sprypassword1 = new Spry.Widget.ValidationPassword("sprypassword2");
+var sprypassword2 = new Spry.Widget.ValidationPassword("sprypassword1");
+</script>
+</body>
+</html>
--- admin/users.php
+++ admin/users.php
@@ -1,3 +1,57 @@
+<?php require_once('../Connections/localhost.php'); ?>
+<?php
+if (!function_exists("GetSQLValueString")) {
+function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") 
+{
+  if (PHP_VERSION < 6) {
+    $theValue = get_magic_quotes_gpc() ? stripslashes($theValue) : $theValue;
+  }
+
+  $theValue = function_exists("mysql_real_escape_string") ? mysql_real_escape_string($theValue) : mysql_escape_string($theValue);
+
+  switch ($theType) {
+    case "text":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;    
+    case "long":
+    case "int":
+      $theValue = ($theValue != "") ? intval($theValue) : "NULL";
+      break;
+    case "double":
+      $theValue = ($theValue != "") ? doubleval($theValue) : "NULL";
+      break;
+    case "date":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;
+    case "defined":
+      $theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
+      break;
+  }
+  return $theValue;
+}
+}
+
+$maxRows_rs_users = 10;
+$pageNum_rs_users = 0;
+if (isset($_GET['pageNum_rs_users'])) {
+  $pageNum_rs_users = $_GET['pageNum_rs_users'];
+}
+$startRow_rs_users = $pageNum_rs_users * $maxRows_rs_users;
+
+mysql_select_db($database_localhost, $localhost);
+$query_rs_users = "SELECT username, first_name, last_name, email FROM tb_users ORDER BY username ASC";
+$query_limit_rs_users = sprintf("%s LIMIT %d, %d", $query_rs_users, $startRow_rs_users, $maxRows_rs_users);
+$rs_users = mysql_query($query_limit_rs_users, $localhost) or die(mysql_error());
+$row_rs_users = mysql_fetch_assoc($rs_users);
+
+if (isset($_GET['totalRows_rs_users'])) {
+  $totalRows_rs_users = $_GET['totalRows_rs_users'];
+} else {
+  $all_rs_users = mysql_query($query_rs_users);
+  $totalRows_rs_users = mysql_num_rows($all_rs_users);
+}
+$totalPages_rs_users = ceil($totalRows_rs_users/$maxRows_rs_users)-1;
+?>
 <!--
 To change this template, choose Tools | Templates
 and open the template in the editor.
@@ -9,10 +63,36 @@ and open the template in the editor.
         <title></title>
     </head>
     <body>
-        USUARIOS
-        <?php
+    <p>USUARIOS
+      <?php
         // put your code here
         
         ?>
+    </p>
+    <table width="338" border="1">
+      <tr>
+        <td width="47">Usuario</td>
+        <td width="51" align="center">Nombre</td>
+        <td width="57" align="center">Apellidos</td>
+        <td width="33" align="center">Email</td>
+        <td width="116" align="center">Acción</td>
+      </tr>
+      <?php do { ?>
+      <tr>
+        <td height="26"><?php echo $row_rs_users['username']; ?></td>
+        <td><?php echo $row_rs_users['first_name']; ?></td>
+        <td><?php echo $row_rs_users['last_name']; ?></td>
+        <td><?php echo $row_rs_users['email']; ?></td>
+        <td align="center"><a href="edit_user.php?username=<?php echo $row_rs_users['username']; ?>">Editar</a> | <a href="users.php?username=<?php echo $row_rs_users['username']; ?>">Eliminar</a></td>
+      </tr>
+      <?php } while ($row_rs_users = mysql_fetch_assoc($rs_users)); ?>
+      <tr>
+        <td height="26" colspan="5" align="center"><a href="add_user.php">Agregar</a></td>
+      </tr>
+    </table>
+    <p>&nbsp;</p>
     </body>
 </html>
+<?php
+mysql_free_result($rs_users);
+?>
