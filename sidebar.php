33845732dfa2d6c62f05242840adec43d9153311
Kevin Porras <kporras07@gmail.com>
2013-05-26 16:47:20 -0600

    sidebar in progress

---
+++ sidebar.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+<link href="css/estilos.css" rel="stylesheet" type="text/css" media="screen" />
+<link href="css/sidebar.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+
+<div id="sidebar" class="sidebar">
+
+	<div class="banner">
+    
+    </div>
+	<div class="games-sidebar">
+    
+        <h1 id="title-sidebar">
+            MÁS POPULARES
+        </h1>
+        
+            <div class="game-sidebar">
+                
+                <div class="game-picture">
+                    <img src="imagenes/not-available.gif" height="110px" width="185px" alt="Imagen Juego"/>
+                </div>
+                <div class="game-title">NOMBRE JUEGO</div>
+            </div>
+            
+            <div class="game-sidebar">
+                
+                <div class="game-picture">
+                    <img src="imagenes/not-available.gif" height="110px" width="185px" alt="Imagen Juego"/>
+                </div>
+                <div class="game-title">NOMBRE JUEGO</div>
+            </div>
+            
+            <div class="game-sidebar">
+                
+                <div class="game-picture">
+                    <img src="imagenes/not-available.gif" height="110px" width="185px" alt="Imagen Juego"/>
+                </div>
+                <div class="game-title">NOMBRE JUEGO</div>
+            </div>
+            
+            
+    
+    </div>
+    
+	<div id="sidebar-prevnext"></div>
+
+
+</div>
+
+</body>
+</html>
