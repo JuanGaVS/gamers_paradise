d21ddada48ad8809250beb6ef399e183628febd9
Kevin Porras <kporras07@gmail.com>
2013-05-26 19:22:49 -0600

    cycle added

---
+++ js/sidebar.js
@@ -0,0 +1,21 @@
+// JavaScript Document
+
+var slideActual = 1;
+
+var slide1 = $('#banner-section1');
+var slide2 = $('#games-section1');
+var slide3 = $('#banner-section2');
+var slide4 = $('#games-section2');
+var slide5 = $('#banner-section3');
+var slide6 = $('#games-section3');
+
+
+$(document).ready(function() {
+
+    $('.games-sidebar').hide();
+	$('.banner').hide();
+	$('#banner-section1').show();
+
+
+
+});
--- sidebar.php
+++ sidebar.php
@@ -4,9 +4,6 @@ require_once 'dal/GameDAL.php';
 $gDAL = new GameDAL();
 
 $games = $gDAL->getGames();
-
-
-
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -25,16 +22,7 @@ $games = $gDAL->getGames();
 
                 </div>
             </a>
-            <a href="#" target="_blank">
-                <div class="banner" id="banner-section2">
-
-                </div>
-            </a>
-            <a href="#" target="_blank">
-                <div class="banner" id="banner-section3">
-
-                </div>
-            </a>
+            
             <div class="games-sidebar" id="games-section1">
 
                 <h1 id="title-sidebar">
@@ -57,15 +45,14 @@ for ($actualIndex = 0; $actualIndex < 3; $actualIndex++) {
 
 
             </div>
+            
+            <a href="#" target="_blank">
+                <div class="banner" id="banner-section2">
 
-
-            <div id="sidebar-bottom">
-                <div id="sidebar-prevnext">
-                    <div id="prev"></div>
-                    <div id="next"></div>
                 </div>
-            </div>
-
+            </a>
+            
+            
             <div class="games-sidebar" id="games-section2">
 
                 <h1 id="title-sidebar">
@@ -89,6 +76,23 @@ for ($actualIndex = 3; $actualIndex < 6; $actualIndex++) {
 
 
             </div>
+            
+            <a href="#" target="_blank">
+                <div class="banner" id="banner-section3">
+
+                </div>
+            </a>
+            
+
+
+            <div id="sidebar-bottom">
+                <div id="sidebar-prevnext">
+                    <div id="prev"></div>
+                    <div id="next"></div>
+                </div>
+            </div>
+
+            
 
             <div class="games-sidebar" id="games-section3">
 
@@ -120,6 +124,10 @@ for ($actualIndex = 6; $actualIndex < 9; $actualIndex++) {
 
 
         </div>
+
+        <script src="js/jquery-1.9.1.min.js" type="text/javascript"></script>
+        <script src="js/jquery.cycle.all.js" type="text/javascript"></script>
+        <script src="js/sidebar.js" type="text/javascript"></script>
 
     </body>
 </html>
