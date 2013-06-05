7161b5c922257f619e2f3081af515d32675a63de
Kevin Porras <kporras07@gmail.com>
2013-06-05 02:16:57 -0600

    merge

--- calls.php
+++ calls.php
@@ -1,38 +1,6 @@
 <?php
 
-switch ($_POST['method']) {
-    case 'mayor':
-        mayor($_POST['numero1']);
-        break;
-    case 'sendSurvey':
-
-        $respuestas = $_POST['answers'];
-        sendSurvey();
-
-        $respuestas = $_POST('answers');
-        //sendSurvey( );
-
-        break;
-    case 'saveUserGame':
-
-       
-
-        break;
-}//Fin de switch.
-
-function mayor($numero1) {
-    echo $numero1;
-}
-
-//Fin de function mayor.
-
-function sendSurvey() {
-    echo "ENTRO";
-}
-
-//Fin de function mayor.
-
-require_once('entities/GameUserDAL.php');
+require_once('entities/GameContestantDAL.php');
 
 $gameUserDAL = new GameUserDAL();
 	
